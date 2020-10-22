157d115f96044eec0d5585a57255432d20e07bd4
Chris Kalama <chriskalama96@gmail.com>
2020-10-21 23:23:56 -0500

    added var team and fs.writeFile

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+var team = []
+
+fs.writeFile(outputPath, render(team))
+//have to call render and pass Team. 
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
